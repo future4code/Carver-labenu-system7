4d9ba63eb0427e645251c97a51b127fd6751c39f
LEANDROMAIERO <leandro.gmaiero@gmail.com>
2022-02-17 19:02:35 -0300

    turma adicionada

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { app } from './app';
+import { createClass } from './endpoints/createClass';
 import { createStudent } from './endpoints/createStudent';
 
 app.post('/student', createStudent);
+app.post('/class', createClass)
