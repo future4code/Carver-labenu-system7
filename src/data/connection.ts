408a6f94f66aeab3521d0a359617815135aca382
0dayIg0r <igormedeiros.f@gmail.com>
2022-02-20 14:27:37 -0300

    correcao create teacher

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -6,10 +6,11 @@ dotenv.config();
 export const connection = knex({
   client: "mysql",
   connection: {
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+         host: process.env.DB_HOST,
+         user: process.env.DB_USER,
+         password: process.env.DB_PASS,
+         database: process.env.DB_NAME,
+         port: 3306,
+         multipleStatements: true
 }
 });
--- src/endpoints/createTeacher.ts
+++ src/endpoints/createTeacher.ts
@@ -1,13 +1,24 @@
 import {Request, Response} from 'express'
 import { Teacher } from '../classes/teacherClass'
+import { connection } from '../data/connection'
 
 let errorCode = 0
-export const createTeacher = async (req:Request, res:Response): Promise<void> =>{
+export const createTeacher = async (req:Request, res:Response) =>{
     try{
         let {nome, email, data_nasc, turma_id} = req.body
         const id = Date.now().toString()
         data_nasc = data_nasc.split('/').reverse().join('-')
         
+        const newTeacher = new Teacher(id, nome, email, data_nasc, turma_id)
+
+        await connection('Docente')
+        .insert({
+            id: newTeacher.getId(),
+            nome: newTeacher.getNome(),
+            email: newTeacher.getEmail(),
+            data_nasc: newTeacher.getDataNasc(),
+            turma_id: newTeacher.getTurmaId()
+        })
         if (!nome || !email || !data_nasc || !turma_id){
             errorCode = 422
             res.status(errorCode).send('Preencha todos os campos')
